9bff7655ce89c28ccb9d74bdddabe15fbebec88b
Volodymyr Dudka <tanetti22@gmail.com>
2022-07-28 20:31:14 +0300

    Final release

--- src/js/gallery-generator/gallery-generator.js
+++ src/js/gallery-generator/gallery-generator.js
@@ -5,12 +5,13 @@ export default class GalleryGenerator {
   #key;
   #refs;
   #onSuccess;
-  #onError;
+  #onFailure;
   #simpleLightboxInstance;
   #scrollToNewResults;
   #infiniteScroll;
   #currentTotalHits;
-  #resultsHeight;
+  #scrollObserver;
+  #aosAnimation;
 
   constructor() {
     this.#baseURL = 'https://pixabay.com/api/';
@@ -30,13 +31,14 @@ export default class GalleryGenerator {
     this.perPage = 40;
 
     this.#onSuccess = null;
-    this.#onError = null;
+    this.#onFailure = null;
     this.#simpleLightboxInstance = null;
     this.#scrollToNewResults = true;
     this.#infiniteScroll = true;
+    this.#aosAnimation = '';
 
     this.#currentTotalHits = 0;
-    this.#resultsHeight = 0;
+    this.#scrollObserver = null;
   }
 
   init(options) {
@@ -48,10 +50,11 @@ export default class GalleryGenerator {
       safesearch = true,
       perPage = 40,
       onSuccess = null,
-      onError = null,
+      onFailure = null,
       simpleLightboxInstance = null,
       scrollToNewResults = true,
       infiniteScroll = true,
+      aosAnimation = '',
     } = options;
 
     this.imageType = imageType;
@@ -59,44 +62,47 @@ export default class GalleryGenerator {
     this.safesearch = safesearch;
     this.perPage = perPage;
     this.#onSuccess = onSuccess;
-    this.#onError = onError;
+    this.#onFailure = onFailure;
     this.#simpleLightboxInstance = simpleLightboxInstance;
     this.#scrollToNewResults = scrollToNewResults;
     this.#infiniteScroll = infiniteScroll;
+    this.#aosAnimation = aosAnimation;
   }
 
   async start(isTrigereredByScroll = false) {
-    if (!this.#refs.inputField) return this.#errorNotification('Input field not specified!');
-    if (!this.#refs.galleryContainer) return this.#errorNotification('Gallery container not specified!');
+    if (!this.#refs.inputField) return this.#failureNotification('Input field not specified!');
+    if (!this.#refs.galleryContainer) return this.#failureNotification('Gallery container not specified!');
 
     const sanitizedQuery = this.#refs.inputField.value.trim();
 
     if (!sanitizedQuery) return;
 
     if (sanitizedQuery === this.query) {
-      if (this.#currentTotalHits === 0) return this.#errorNotification('Sorry, there are no images matching your search query. Please try again.');
+      if (this.#currentTotalHits === 0) return this.#failureNotification('Sorry, there are no images matching your search query. Please try again.');
 
       const totalHitsRendered = this.currentPage * this.perPage;
-      if (totalHitsRendered >= this.#currentTotalHits) return this.#errorNotification("We're sorry, but you've reached the end of search results.");
+      if (totalHitsRendered >= this.#currentTotalHits) return this.#failureNotification("We're sorry, but you've reached the end of search results.");
     } else {
       this.query = sanitizedQuery;
       this.currentPage = 1;
       this.#refs.galleryContainer.innerHTML = '';
     }
 
+    if (this.#scrollObserver) this.#scrollObserver.disconnect();
+
     this.#toggleLoaderVisibility();
+    const galleryDimensions = this.#refs.galleryContainer.getBoundingClientRect();
 
     try {
       this.#renderData(await this.#fetchData());
     } catch (error) {
-      this.#errorNotification(error.message);
+      this.#failureNotification(error.message);
     }
 
+    if (this.#scrollToNewResults && !isTrigereredByScroll && this.currentPage > 2) this.#scrollToNewResult(galleryDimensions);
     this.#toggleLoaderVisibility();
 
-    if (this.#infiniteScroll) this.#createInfiniteScrollHandler();
-
-    if (this.#scrollToNewResults && !isTrigereredByScroll && this.currentPage >= 2) this.#scrollToNewResult();
+    if (this.#infiniteScroll) this.#createInfiniteScrollObserver();
   }
 
   async #fetchData() {
@@ -117,25 +123,20 @@ export default class GalleryGenerator {
     const { hits, totalHits } = data;
 
     this.#currentTotalHits = totalHits;
-    if (totalHits === 0) return this.#errorNotification('Sorry, there are no images matching your search query. Please try again.');
+    if (totalHits === 0) return this.#failureNotification('Sorry, there are no images matching your search query. Please try again.');
 
     this.#refs.galleryContainer.insertAdjacentHTML('beforeend', this.#createGalleryMarkup(hits));
 
-    if (this.currentPage === 1) {
-      this.#successNotification(`Hooray! We found ${totalHits == 500 ? '500+' : totalHits} images.`);
-      this.#resultsHeight = this.#refs.galleryContainer.getBoundingClientRect();
-    }
-
-    this.currentPage += 1;
-
+    if (this.currentPage === 1) this.#successNotification(`Hooray! We found ${totalHits == 500 ? '500+' : totalHits} images.`);
     if (this.#simpleLightboxInstance) this.#simpleLightboxInstance.refresh();
+    this.currentPage += 1;
   }
 
   #createGalleryMarkup(imagesData) {
     return imagesData
       .map(
         ({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) =>
-          `<a class="card" href="${largeImageURL}">
+          `<a class="card" href="${largeImageURL}" ${this.#aosAnimation ? `data-aos=${this.#aosAnimation}` : ''}>
             <img class="card__image" src="${webformatURL}" alt="${tags}" width="220" height="150" loading="lazy"/>
             <div class="card__info">
               <p class="card__info-item">
@@ -166,10 +167,10 @@ export default class GalleryGenerator {
     this.#onSuccess(message);
   }
 
-  #errorNotification(message) {
-    if (!this.#onError) return alert(`ERROR!!!\n${message}`);
+  #failureNotification(message) {
+    if (!this.#onFailure) return alert(`FAILURE!!!\n${message}`);
 
-    this.#onError(message);
+    this.#onFailure(message);
   }
 
   #toggleLoaderVisibility() {
@@ -177,22 +178,21 @@ export default class GalleryGenerator {
     this.#refs.galleryLoader.classList.toggle('is-hidden');
   }
 
-  #scrollToNewResult() {
-    window.scrollBy(0, this.#resultsHeight.height);
-    console.log('Scroll');
+  #scrollToNewResult({ height }) {
+    window.scrollTo(0, height);
   }
 
-  #createInfiniteScrollHandler() {
-    const observer = new IntersectionObserver(this.#onInfiniteScrollIntersection.bind(this), { threshold: 0.1 });
+  #createInfiniteScrollObserver() {
+    this.#scrollObserver = new IntersectionObserver(this.#onScrollIntersection.bind(this), { threshold: 0.1 });
     const lastLoadedImage = this.#refs.galleryContainer.lastElementChild;
 
-    lastLoadedImage && observer.observe(lastLoadedImage);
+    lastLoadedImage && this.#scrollObserver.observe(lastLoadedImage);
   }
 
-  #onInfiniteScrollIntersection(entries, observer) {
+  #onScrollIntersection(entries, scrollObserver) {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        observer.unobserve(entry.target);
+        scrollObserver.unobserve(entry.target);
         this.start(true);
       }
     });
--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@ import '../sass/index.scss';
 import { Notify } from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import Aos from 'aos';
+import 'aos/dist/aos.css';
 import GalleryGenerator from './gallery-generator/gallery-generator';
 
 Notify.init({
@@ -21,22 +23,38 @@ const gallery = new SimpleLightbox('div.gallery__content a', {
   alertError: false,
 });
 
+Aos.init();
+
 const galleryGenerator = new GalleryGenerator();
 galleryGenerator.init({
   onSuccess: Notify.success,
-  onError: Notify.failure,
+  onFailure: Notify.failure,
   simpleLightboxInstance: gallery,
+  aosAnimation: 'zoom-in-up',
 
-  // ------ Default values ------
-  // imageType: 'photo',
-  // orientation: 'horizontal',
-  // safesearch: true,
-  // perPage: 40,
-  // onSuccess: null,
-  // onError: null,
-  // simpleLightboxInstance: null,
-  // scrollToNewResults: true,
-  // infiniteScroll = true,
+  // ------ Options and default values ------
+
+  // Pixabay query settings:
+  //    imageType: 'photo', (STRING)
+  //    orientation: 'horizontal', (STRING)
+  //    safesearch: true, (BOOLEAN)
+  //    perPage: 40, (NUMBER)
+
+  // Callback for notification (Use Alet() default):
+  //    onSuccess: null, (CALLBACK)
+  //    onFailure: null, (CALLBACK)
+
+  // Apply initialized SimpleLightbox instance:
+  //    simpleLightboxInstance: null, (INSTANCE)
+
+  // Enable or disable autoscroll to new result after search:
+  //    scrollToNewResults: true, (BOOLEAN)
+
+  // Enable or disable infinite scroll:
+  //    infiniteScroll: true, (BOOLEAN)
+
+  // Apply AOS animation type ('fade-up', 'flip-left', 'zoom-in' etc...):
+  //    aosAnimation: '', (STRING)
 });
 
 const refs = {
