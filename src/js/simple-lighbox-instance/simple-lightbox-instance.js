0b0fd83034139754b0deddb3245dafe500a6a006
Volodymyr Dudka <tanetti22@gmail.com>
2022-08-02 15:35:59 +0300

    Add hide on scroll for header

--- src/js/simple-lighbox-instance/simple-lightbox-instance.js
+++ src/js/simple-lighbox-instance/simple-lightbox-instance.js
@@ -2,7 +2,7 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 export const gallery = new SimpleLightbox('div.gallery__content a', {
-  maxZoom: 2,
+  maxZoom: 4,
   overlayOpacity: 0.85,
   captionsData: 'alt',
   disableRightClick: true,
